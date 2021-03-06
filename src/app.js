9f66880927185b71a1cf60442433796c9260c59f
Arwinder Singh <arwindersinghh@gmail.com>
2021-03-06 03:28:53 -0500

    barebones homepage

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
+import { Router } from "react-router-dom";
 import React from "react";
 import { Provider } from "react-redux";
+import Homepage from "./components/Homepage";
 import store from './store'
+import history from "./history";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>Hello World!</div>
+      <Router history={history}>
+      <Homepage />
+      </Router>
     </Provider>
   );
 };
---
+++ src/components/Homepage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+
+const Homepage = (props) => {
+    console.log(props);
+
+  return (
+    <div style={{height:"100vh"}}>
+      <h3 style={{ width:"100vw", color:"white", backgroundColor:"purple", textAlign:"center"}}>Welcome to escape-room</h3>
+
+        <Link to="/game1"> Game 1 </Link>
+        <hr />
+        <Link to="/game2"> Game 2 </Link>
+        <hr />
+        <Link to="/game3"> Game 3 </Link>
+
+    <h3 style={{ position:"absolute", bottom:"0", width:"100vw", color:"white", backgroundColor:"purple", textAlign:"center" }}> Designed by VARKS </h3>
+
+    </div>
+  )
+}
+
+
+export default Homepage;

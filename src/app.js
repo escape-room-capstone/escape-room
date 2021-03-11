6d8b71d1519886055a4b70cd059854bbb49ad06b
Vanessa Lima <limawhe@gmail.com>
2021-03-11 01:05:13 -0500

    added modals and puzzles local storage

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
-
-import BobaFett from "./components/SteveGame/BobaFett";
+import BobaFett from './components/SteveGame/BobaFett';
 
 import { Router, Route, Redirect } from 'react-router-dom';
 import React from 'react';
@@ -14,48 +13,51 @@ import { HauntedRoom3 } from './components/Haunted/HauntedRoom3';
 import { HauntedIntro } from './components/Haunted/HauntedIntro';
 import { Success } from './components/Haunted/HauntedR2Success';
 
-import GameFour from './components/GameFour/GameFour.js';
-import LivingRoom from './components/GameFour/LivingRoom.js';
-import BackRoom from './components/GameFour/BackRoom';
-import HerRoom from './components/GameFour/HerRoom';
-import HisRoom from './components/GameFour/HisRoom';
-import Attic from './components/GameFour/Attic';
+// import GameFour from './components/GameFour/GameFour.js';
+// import RoomOne from './components/GameFour/HerRoom';
+// import RoomTwo from './components/GameFour/HisRoom';
+
+import Trapped from './components/Trapped/Trapped';
+import LivingRoom from './components/Trapped/LivingRoom';
+import BackRoom from './components/Trapped/BackRoom';
+import Attic from './components/Trapped/Attic';
+import RoomOne from './components/Trapped/RoomOne';
+import RoomTwo from './components/Trapped/RoomTwo';
 
 import BankGameLevel2 from './components/BankGame/BankGameLevel2';
 import BankGameLevel3 from './components/BankGame/BankGameLevel3';
 import BankGameStart from './components/BankGame/BankGameStart';
 import BankGameVictory from './components/BankGame/BankGameVictory';
 
-
-
 const App = () => {
   return (
     <Provider store={store}>
       <Router history={history}>
         <Route exact path="/" component={Homepage} />
-        <Route exact path ="/Bobafett" component={BobaFett} />
+
+        <Route exact path="/Bobafett" component={BobaFett} />
+
         <Route exact path="/haunted/intro" component={HauntedIntro} />
         <Route exact path="/haunted/room1" component={HauntedRoom1} />
         <Route exact path="/haunted/room2" component={HauntedRoom2} />
         <Route exact path="/haunted/room3" component={HauntedRoom3} />
         <Route exact path="/haunted/room2/success" component={Success} />
 
-        <Route exact path="/game4" component={GameFour} />
-        <Route exact path="/game4/livingroom" component={LivingRoom} />
-        <Route exact path="/game4/herroom" component={HerRoom} />
-        <Route exact path="/game4/backroom" component={BackRoom} />
-        <Route exact path="/game4/hisroom" component={HisRoom} />
-        <Route exact path="/game4/attic" component={Attic} />
+        <Route exact path="/trapped" component={Trapped} />
+        <Route exact path="/livingroom" component={LivingRoom} />
+        <Route exact path="/room1" component={RoomOne} />
+        <Route exact path="/room2" component={RoomTwo} />
+        <Route exact path="/backroom" component={BackRoom} />
+        <Route exact path="/attic" component={Attic} />
 
         <Route exact path="/Bankgame" component={BankGameStart} />
         <Route exact path="/Bankgame2" component={BankGameLevel2} />
         <Route exact path="/Bankgame3" component={BankGameLevel3} />
         <Route exact path="/BankgameVictory" component={BankGameVictory} />
-        <Redirect to="/" />
 
+        {/* <Redirect to="/" /> */}
       </Router>
     </Provider>
-
   );
 };
 

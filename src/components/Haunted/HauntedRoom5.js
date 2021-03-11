a2ba539d6f7d52fef33040403127ec954a32558d
Kate Quinn <katequinn7@gmail.com>
2021-03-11 15:04:39 -0500

    room5

---
+++ src/components/Haunted/HauntedRoom5.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import {
+  Stage,
+  Layer,
+  Star,
+  Text,
+  Circle,
+  Group,
+  Image,
+  Rect,
+} from 'react-konva';
+import Konva from 'konva';
+import { Redirect, Link } from 'react-router-dom';
+import { Portal } from './Portal';
+import useImage from 'use-image';
+import '../../../public/css/HauntedRoom.css';
+import TypeWriterEffect from 'react-typewriter-effect';
+
+export const HauntedRoom5 = (props) => {
+  return <div>room5</div>;
+};

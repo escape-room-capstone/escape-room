cab1379318d0d4890c454a26385f781878eabf13
Kate Quinn <katequinn7@gmail.com>
2021-03-19 20:07:21 -0400

    choose puzzle form update

--- src/components/ChoosePuzzleForm.js
+++ src/components/ChoosePuzzleForm.js
@@ -1,5 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
-export const ChoosePuzzleForm = (props) => {
-  return <div>Puzzle Form</div>;
+const _ChoosePuzzleForm = (props) => {
+  return (
+    <div>
+      <h2>Choose ${props.game.numPuzzles} puzzles for your game</h2>
+      <div>
+        {props.puzzles.map((puzzle) => (
+          <div>{puzzle.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
+
+export const ChoosePuzzleForm = connect((state) => state)(_ChoosePuzzleForm);

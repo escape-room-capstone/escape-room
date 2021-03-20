91c72ea80f6a82e0f419903d5bd41e5fc83d147b
Vanessa Lima <limawhe@gmail.com>
2021-03-20 13:02:19 -0400

    small edits to seed data

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,6 @@ const config = {
   logging: false,
 };
 
-
 if (process.env.LOGGING === 'true') {
   delete config.logging;
 }
@@ -23,3 +22,28 @@ const db = new Sequelize(
   config
 );
 module.exports = db;
+
+//adding this from nicks comment about heroku being updated:
+
+// let config;
+// if (process.env.DATABASE_URL) {
+//   config = {
+//     logging: false,
+//     operatorsAliases: false,
+//     dialect: "postgres",
+//     protocol: "postgres",
+//     ssl: true,
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   }
+// } else {
+//   config = {
+//     logging: false,
+//     operatorsAliases: false,
+//   }
+// }
+// const client = new Sequelize(dbUrl, config)
--- src/components/HouseOfRiddlez/Attic.js
+++ src/components/HouseOfRiddlez/Attic.js
@@ -4,8 +4,9 @@ import ReactModal from 'react-modal';
 import LetterBank from './LetterBank';
 import { connect, useSelector, useDispatch } from 'react-redux';
 import { setPuzzles } from '../../store/puzzles.js';
+import { fetchGame } from '../../store/game';
 
-const Attic = ({ puzzles }, props) => {
+const Attic = (props) => {
   const [atticPuzzle1, setAtticPuzzle1] = useState({
     riddle: '',
     solution: '',
@@ -18,17 +19,20 @@ const Attic = ({ puzzles }, props) => {
     clue2: 'clue2',
     clue3: 'i am attic clue3',
   });
-  setPuzzles();
+
   const [count, setCount] = useState(0);
 
-  // const modal = document.getElementById('modal-content');
-  // window.onClick = function (event) {
-  //   if (event.target !== modal) {
-  //     modal.portalClassName.style.display = 'none';
-  //   }
-  // };
-  console.log('state=>', puzzles);
-  console.log('props=>', props);
+  useEffect(() => {
+    //get game related puzzles with hard coded gameId for now for testing purposes
+    props.getGame(2);
+  }, []);
+  useEffect(() => {
+    //get all puzzles ever made
+    props.getPuzzles();
+  }, []);
+
+  console.log('atticProps=>', props);
+
   return (
     <div className="container">
       <LetterBank atticClue1={atticClues.clue1} />
@@ -49,39 +53,6 @@ const Attic = ({ puzzles }, props) => {
         </div>
       </ReactModal>
 
-      {/* <ReactModal
-        overlayClassName="OverlayAttic"
-        className="modal"
-        isOpen={puzzle2.showModal}
-        onRequestClose={() => setPuzzle2({ showModal: false })}
-      >
-        <div className="modal-content">
-          ~~~~~THIS IS PUZZLLEEE TWO~~~~~~
-          <button
-            className="close"
-            onClick={() => setPuzzle2({ showModal: false })}
-          >
-            x
-          </button>
-        </div>
-      </ReactModal>
-      <ReactModal
-        overlayClassName="OverlayAttic"
-        className="modal"
-        isOpen={puzzle3.showModal}
-        onRequestClose={() => setPuzzle3({ showModal: false })}
-      >
-        <div className="modal-content">
-          ~~~~~THIS IS PUZZLLEEE THREEEE~~~~~~
-          <button
-            className="close"
-            onClick={() => setPuzzle3({ showModal: false })}
-          >
-            x
-          </button>
-        </div>
-      </ReactModal> */}
-
       <div className="background" id="attic">
         <button onClick={() => setAtticPuzzle1({ showModal: true })}>
           Puzzle 1
@@ -98,15 +69,16 @@ const Attic = ({ puzzles }, props) => {
   );
 };
 
-// const mapToState = (state) => {
-//   return state;
-// };
+const mapToState = (state) => {
+  return state;
+};
 
-// const mapToDispatch = (dispatch) => {
-//   return {
-//     getPuzzles: () => dispatch(setPuzzles()),
-//   };
-// };
+const mapToDispatch = (dispatch) => {
+  return {
+    getGame: (gameId) => dispatch(fetchGame(gameId)),
+    getPuzzles: () => dispatch(setPuzzles()),
+  };
+};
 
-// export default connect(mapToState, mapToDispatch)(Attic);
-export default Attic;
+export default connect(mapToState, mapToDispatch)(Attic);
+//export default Attic;

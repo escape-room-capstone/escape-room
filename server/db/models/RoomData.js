e4103bb3438e214d77671e97f6301abf4c55ae96
Kate Quinn <katequinn7@gmail.com>
2021-03-31 23:44:02 -0400

    updated roomdata model to include puzzle-text

--- server/db/models/RoomData.js
+++ server/db/models/RoomData.js
@@ -1,24 +1,28 @@
 const db = require('../db');
-const { Sequelize } = require('sequelize');
+const { Sequelize, TEXT } = require('sequelize');
 const { STRING, ARRAY, INTEGER } = Sequelize;
 
 const RoomData = db.define('roomdata', {
-    top: {
-        type: Sequelize.STRING,
-        defaultValue: "150"
-      },
-    left: {
-        type: Sequelize.STRING,
-        defaultValue: "600"
-      },
-      width: {
-        type: Sequelize.STRING,
-        defaultValue: "70"
-      },
-      height: {
-        type: Sequelize.STRING,
-        defaultValue: "20"
-      }
+  top: {
+    type: Sequelize.STRING,
+    defaultValue: '150',
+  },
+  left: {
+    type: Sequelize.STRING,
+    defaultValue: '600',
+  },
+  width: {
+    type: Sequelize.STRING,
+    defaultValue: '70',
+  },
+  height: {
+    type: Sequelize.STRING,
+    defaultValue: '20',
+  },
+  puzzleText: {
+    type: TEXT,
+    allowNull: true,
+  },
 });
 
 module.exports = RoomData;

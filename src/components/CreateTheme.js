d0f3a76f4de7d68e8f77088c4566526a8e8d1f39
Kate Quinn <katequinn7@gmail.com>
2021-04-03 01:13:29 -0400

    deleted old code

--- src/components/CreateTheme.js
+++ src/components/CreateTheme.js
@@ -4,26 +4,6 @@ import { connect } from 'react-redux';
 import '../../public/CSS/CreateTheme.css';
 
 const _CreateTheme = (props) => {
-  const [data, setData] = useState({});
-  const uploadHandler = async (e) => {
-    setData(e.target.files[0]);
-    // const formData = new FormData();
-    // console.log(e.target.files[0], 'e.target.files[0]');
-    // formData.append('file', e.target.files[0]);
-    // console.log(formData, 'form data');
-  };
-  //   const handleFormSubmit = async (e) => {
-  //     e.preventDefault();
-  //     const config = {
-  //       headers: {
-  //         'content-type': 'multipart/form-data',
-  //       },
-  //     };
-  //     const formData = new FormData();
-  //     formData.append('file', data);
-  //     const config = { headers: { 'content-type': 'multipart/form-data' } };
-  //     await axios.post('/api/upload', formData, config);
-  //   };
   return (
     <div id="create-theme">
       <h1>Create Theme</h1>

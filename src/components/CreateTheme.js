d1300dfaef9f96358b834623d19450577f5d7025
Kate Quinn <katequinn7@gmail.com>
2021-04-03 01:33:19 -0400

    css for createtheme page

--- src/components/CreateTheme.js
+++ src/components/CreateTheme.js
@@ -6,26 +6,29 @@ import '../../public/CSS/CreateTheme.css';
 const _CreateTheme = (props) => {
   return (
     <div id="create-theme">
-      <h1>Create Theme</h1>
-      <form
-        action={`/api/users/${props.auth.id}/themes`}
-        encType="multipart/form-data"
-        method="POST"
-      >
-        <input
-          name="theme"
-          type="text"
-          placeholder="enter a theme name"
-        ></input>
-        <input
-          //   onChange={(e) => uploadHandler(e)}
-          className="theme"
-          name="images"
-          type="file"
-          multiple="multiple"
-        ></input>
-        <button type="submit">SUBMIT</button>
-      </form>
+      <div>
+        <h1>Create Theme</h1>
+        <form
+          action={`/api/users/${props.auth.id}/themes`}
+          encType="multipart/form-data"
+          method="POST"
+        >
+          <input
+            name="theme"
+            type="text"
+            placeholder="enter a theme name"
+          ></input>
+          <input
+            //   onChange={(e) => uploadHandler(e)}
+            className="theme"
+            name="images"
+            type="file"
+            multiple="multiple"
+          ></input>
+          <br></br>
+          <button type="submit">SUBMIT</button>
+        </form>
+      </div>
     </div>
   );
 };

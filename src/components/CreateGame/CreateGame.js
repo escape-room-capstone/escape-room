3beda40d6e18895d9e900c82698c2733aa70d555
Kate Quinn <katequinn7@gmail.com>
2021-04-09 17:29:33 -0400

    div fix

--- src/components/CreateGame/CreateGame.js
+++ src/components/CreateGame/CreateGame.js
@@ -136,64 +136,8 @@ const CreateGame = (props) => {
   };
   console.log(puzzleArray, 'puzzleArray');
   return (
-
+    <>
       <Navbar />
-
-    <div id="create-game">
-      <h1 style={{ color: '#e6e6e6' }}> Theme : {theme.name} </h1>
-      <div>
-        <label>
-          Title of game :
-          <input
-            style={{ width: '200px', marginLeft: '10px' }}
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            type="text"
-          />
-        </label>
-        <label>
-          Description :
-          <input
-            style={{ width: '200px', marginLeft: '10px' }}
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            type="text"
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Set initial timer for the game :
-          <input
-            name="minutes"
-            min={0}
-            value={Math.floor(timer / 60)}
-            style={{ width: '50px', marginLeft: '10px' }}
-            onChange={(e) => convertAndSetTime(e)}
-            type="number"
-          />
-          minute(s)
-          <input
-            name="seconds"
-            min={0}
-            value={timer - Math.floor(timer / 60) * 60}
-            style={{ width: '50px', marginLeft: '10px' }}
-            onChange={(e) => convertAndSetTime(e)}
-            type="number"
-          />
-          second(s)
-        </label>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}
-      >
-        <h3>Select all the puzzles you would like in your game...</h3>
-
 
       <div id="create-game">
         <h1 style={{ color: '#e6e6e6' }}> Theme : {theme.name} </h1>
@@ -222,6 +166,7 @@ const CreateGame = (props) => {
             Set initial timer for the game :
             <input
               name="minutes"
+              min={0}
               value={Math.floor(timer / 60)}
               style={{ width: '50px', marginLeft: '10px' }}
               onChange={(e) => convertAndSetTime(e)}
@@ -230,6 +175,7 @@ const CreateGame = (props) => {
             minute(s)
             <input
               name="seconds"
+              min={0}
               value={timer - Math.floor(timer / 60) * 60}
               style={{ width: '50px', marginLeft: '10px' }}
               onChange={(e) => convertAndSetTime(e)}
@@ -248,54 +194,109 @@ const CreateGame = (props) => {
         >
           <h3>Select all the puzzles you would like in your game...</h3>
 
-          <div className="puzzlesContainer">
-            {puzzles.map((puzzle, idx) => {
-              // const Component = componentMapping[puzzle.name];
-              return (
-                <div
-                  onClick={(e) => handleChange(e, puzzle.id)}
-                  key={puzzle.id}
-                  className={`puzzle ${
-                    puzzleArray.includes(puzzle.id) ? 'selected' : ''
-                  }`}
-                >
-                  {/* puzzle.nickname instead, like "Magic Squares, etc"  ???*/}
-                  <div>
-                    <h3 className="puzzle-name">{puzzle.name}</h3>
-                    <button
-                      onClick={() => {
-                        setPuzzleToShow(puzzle.name);
-                        setShowModal(true);
-                      }}
-                    >
-                      Try Out
-                    </button>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div
-            style={{
-              color: 'red',
-              fontSize: '1.5rem',
-              margin: '1rem auto 2rem auto',
-            }}
-          >
-            {error}
-          </div>
-
-          <button className="submit" onClick={() => submitCreateGame()}>
-            {' '}
-            Submit{' '}
-          </button>
-          <Modal isOpen={showModal}>
+          <div id="create-game">
+            <h1 style={{ color: '#e6e6e6' }}> Theme : {theme.name} </h1>
             <div>
-              <div>{generatePuzzle(puzzleToShow, props)}</div>
-              <div>{success ? 'Nice work!' : ''}</div>
+              <label>
+                Title of game :
+                <input
+                  style={{ width: '200px', marginLeft: '10px' }}
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  type="text"
+                />
+              </label>
+              <label>
+                Description :
+                <input
+                  style={{ width: '200px', marginLeft: '10px' }}
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  type="text"
+                />
+              </label>
             </div>
-            <button onClick={() => setShowModal(false)}>CLOSE</button>
-          </Modal>
+            <div>
+              <label>
+                Set initial timer for the game :
+                <input
+                  name="minutes"
+                  value={Math.floor(timer / 60)}
+                  style={{ width: '50px', marginLeft: '10px' }}
+                  onChange={(e) => convertAndSetTime(e)}
+                  type="number"
+                />
+                minute(s)
+                <input
+                  name="seconds"
+                  value={timer - Math.floor(timer / 60) * 60}
+                  style={{ width: '50px', marginLeft: '10px' }}
+                  onChange={(e) => convertAndSetTime(e)}
+                  type="number"
+                />
+                second(s)
+              </label>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}
+            >
+              <h3>Select all the puzzles you would like in your game...</h3>
+
+              <div className="puzzlesContainer">
+                {puzzles.map((puzzle, idx) => {
+                  // const Component = componentMapping[puzzle.name];
+                  return (
+                    <div
+                      onClick={(e) => handleChange(e, puzzle.id)}
+                      key={puzzle.id}
+                      className={`puzzle ${
+                        puzzleArray.includes(puzzle.id) ? 'selected' : ''
+                      }`}
+                    >
+                      {/* puzzle.nickname instead, like "Magic Squares, etc"  ???*/}
+                      <div>
+                        <h3 className="puzzle-name">{puzzle.name}</h3>
+                        <button
+                          onClick={() => {
+                            setPuzzleToShow(puzzle.name);
+                            setShowModal(true);
+                          }}
+                        >
+                          Try Out
+                        </button>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div
+              style={{
+                color: 'red',
+                fontSize: '1.5rem',
+                margin: '1rem auto 2rem auto',
+              }}
+            >
+              {error}
+            </div>
+
+            <button className="submit" onClick={() => submitCreateGame()}>
+              {' '}
+              Submit{' '}
+            </button>
+            <Modal isOpen={showModal}>
+              <div>
+                <div>{generatePuzzle(puzzleToShow, props)}</div>
+                <div>{success ? 'Nice work!' : ''}</div>
+              </div>
+              <button onClick={() => setShowModal(false)}>CLOSE</button>
+            </Modal>
+          </div>
         </div>
       </div>
     </>

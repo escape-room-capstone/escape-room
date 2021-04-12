035d79f8701266373a3a43a443e4f6af6c06370b
Kate Quinn <katequinn7@gmail.com>
2021-04-12 14:23:57 -0400

    basic about page rendering

---
+++ src/components/About.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../../public/CSS/About.css';
+export const About = (props) => {
+  return (
+    <div id="about">
+      <div className="section column">
+        <h1>Technologies</h1>
+        <div>
+          <img src="/Images/AboutImages/react.png"></img>
+          <img id="konva" src="/Images/AboutImages/reactkonva.png"></img>
+          <img src="/Images/AboutImages/redux.png"></img>
+          <img src="/Images/AboutImages/node.png"></img>
+        </div>
+        <div>
+          <img src="/Images/AboutImages/express.png"></img>
+          <img src="/Images/AboutImages/postgresql.png"></img>
+          <img src="/Images/AboutImages/sequelize.svg"></img>
+        </div>
+      </div>
+      <div className="section column">
+        <h1>Challenges</h1>
+        <ul>
+          <li>
+            Dynamically rendering front-end React components based on user data
+            in database
+          </li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="section column">
+        <h1>Future Enhancements</h1>
+      </div>
+    </div>
+  );
+};
